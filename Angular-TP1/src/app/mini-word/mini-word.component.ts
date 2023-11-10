9f03de1b00160086d25911d3de980affaab883ee
sofienazzabi2 <sofien.azzabi23@gmail.com>
2023-11-10 19:24:40 +0100

    feat:create mini-word

---
+++ Angular-TP1/src/app/mini-word/mini-word.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mini-word',
+  templateUrl: './mini-word.component.html',
+  styleUrls: ['./mini-word.component.css'],
+})
+export class MiniWordComponent implements OnInit {
+  color: string = '#000000';
+  size: number = 16;
+  family: string = 'Arial';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
